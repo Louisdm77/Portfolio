05e25e59746bcf456997ccdf0bcff6f2cffb38c7
Akwaji Dan Uke <danakwaji3@gmail.com>
2025-03-21 21:52:23 +0100

    added animation to my data

--- src/components/myInfo/index.jsx
+++ src/components/myInfo/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import "animate.css";
 
 const MyInfo = () => {
   return (
-    <div className="w-full">
+    <div className="w-full mt-4">
       <div className="text-center  text-gray-400">
-        <h3 className="text-lg mb-4">Hi, i am</h3> 
-        <h2 className="text-2xl font-semibold">Akwaji Dan Uke</h2>
+        <h3 className="text-xl mb-4 animate__animated animate__slideInLeft">
+          Hi, i am
+        </h3>
+        <h2 className="text-3xl font-semibold animate__animated animate__slideInRight">
+          Akwaji Dan Uke
+        </h2>
+      </div>
+      <div>
+        <h3 className="text-4xl font-bold animate__animated animate__zoomIn text-center mt-4 role">
+          FrontEnd Developer
+        </h3>
       </div>
     </div>
   );
